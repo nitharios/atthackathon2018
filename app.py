6c15857b2a2373afa6ceb54c62def7ad3b38103d
Nathan N <Nitharios@users.noreply.github.com>
2018-03-10 02:04:22 -1000

    able to read voice input

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-import requests
+# NOTE: this example requires PyAudio because it uses the Microphone class
 
-from config import SUBSCRIPTION_KEY, PATH, TOKEN_PATH, TRACE_ID
+import speech_recognition as sr
 
-requestHeader = {
-  "Ocp-Apim-Subscription-Key": SUBSCRIPTION_KEY
-}
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+  print("Say something!")
+  audio = r.listen(source)
 
-params = {
-  "from": "en-us",
-  "to": "it-IT",
-  "features": "texttospeech",
-  "api-version": "1.0"
-}
-
-# token = requests.post(TOKEN_PATH, headers=requestHeader)
-# token = str(token.content)[1:-1]
-# token = str(token)
-
-req = requests.get(PATH, headers=requestHeader, params=params)
-
-print(req.content)
+# recognize speech using Google Speech Recognition
+try:
+  # for testing purposes, we're just using the default API key
+  # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+  # instead of `r.recognize_google(audio)`
+  print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+  print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+  print(
+    "Could not request results from Google Speech Recognition service; {0}".format(e))
