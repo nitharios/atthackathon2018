dbb1e943f4f4f8819de294bc97c5ff30ae0f309e
Nathan N <Nitharios@users.noreply.github.com>
2018-03-10 02:50:45 -1000

    able to convert text to specific language

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 # NOTE: this example requires PyAudio because it uses the Microphone class
 
+# Imports the Google Cloud client library
+from os import system
 import speech_recognition as sr
+from google.cloud import translate
+
+# Instantiates a client
+translate_client = translate.Client()
 
 # obtain audio from the microphone
 r = sr.Recognizer()
@@ -14,6 +20,21 @@ try:
   # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
   # instead of `r.recognize_google(audio)`
   print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+  # The text to translate
+  text = r.recognize_google(audio)
+  system('say -v Victoria ' + r.recognize_google(audio))
+
+  # The target language
+  target = 'ja'
+
+  # Translates some text into Russian
+  translation = translate_client.translate(
+      text,
+      target_language=target)
+
+  print(u'Text: {}'.format(text))
+  print(u'Translation: {}'.format(translation['translatedText']))
+
 except sr.UnknownValueError:
   print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
