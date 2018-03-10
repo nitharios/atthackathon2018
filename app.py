6c5786414eefa12ad8f3fab4ce50cf0a71530b2f
Nathan N <Nitharios@users.noreply.github.com>
2018-03-10 08:53:17 -1000

    able to process language and respond

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 
 # Imports the Google Cloud client library
 from os import system
-import speech_recognition as sr
 from google.cloud import translate
+
+import speech_recognition as sr
+import pyttsx3
 
 # Instantiates a client
 translate_client = translate.Client()
@@ -11,6 +13,7 @@ translate_client = translate.Client()
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
+  r.adjust_for_ambient_noise(source)
   print("Say something!")
   audio = r.listen(source)
 
@@ -19,21 +22,30 @@ try:
   # for testing purposes, we're just using the default API key
   # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
   # instead of `r.recognize_google(audio)`
-  print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-  # The text to translate
   text = r.recognize_google(audio)
-  system('say -v Victoria ' + r.recognize_google(audio))
+  # The text to translate
+  print(text)
+  # system('say -v Victoria ' + r.recognize_google(audio))
 
   # The target language
   target = 'ja'
 
-  # Translates some text into Russian
+  # Translates text
   translation = translate_client.translate(
       text,
       target_language=target)
 
-  print(u'Text: {}'.format(text))
-  print(u'Translation: {}'.format(translation['translatedText']))
+  voiceTranslation = format(translation['translatedText'])
+  # voices = engine.getProperty('voices')
+  # for voice in voices:
+    # print(voice.id)
+    # print(voice.languages)
+  engine = pyttsx3.init()
+  engine.setProperty('voice', 'com.apple.speech.synthesis.voice.kyoko')
+  engine.say(voiceTranslation)
+  engine.runAndWait()
+  # print(u'Text: {}'.format(text))
+  # print(u'Translation: {}'.format(translation['translatedText']))
 
 except sr.UnknownValueError:
   print("Google Speech Recognition could not understand audio")
